c4bd3ec8b704b01642cb379ed89ae4c7742940a2
wangzilin <wangzilinn@gmail.com>
2021-06-04 09:12:50 +0800

    merge

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import {store} from './store/index'
 
-const app = createApp(App)
-app.use(store)
-app.mount('#app')
+    const app = createApp(App)
+    app.use(store)
+    app.mount('#app')
 
